a51ab8e66cc53fcfce4e75aef165b0016fdc0023
Hossein Tahmasbi <h.tahmasb@gmail.com>
2018-07-30 15:47:31 +0430

    Test file renamed and modified.

---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import numpy as np
+
+np.random.seed(1)
+a=np.random.randint(5 , size=(3, 4))
+b=np.random.randint(6 , size=(3, 4))
+c = a*b
+print c[:,1]
--- tt.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import numpy as np
-
-a=1.2
-b=2.4
-c = a*b
-print c
